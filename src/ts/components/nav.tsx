9e8581cb0ce2ec0695d1e8ab549e66878cb90841
0xDaine <13499662+0xDaine@users.noreply.github.com>
2022-02-22 23:44:48 +0100

    nav now loading web3

--- src/ts/components/nav.tsx
+++ src/ts/components/nav.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import web3 from 'web3';
+import Web3 from 'web3';
 
-const Header: FC = () => (
-	<header>
-		<nav>
-			<h1>
-				<Link to="/" className="home-link" title="Home">0x574r finance - testnet</Link>
-			</h1>
-			<ul>
-				<li><NavLink to="/stake" className="nav-item" activeClassName="nav-item active">Stake</NavLink></li>
-				<li><NavLink to="/vault" className="nav-item" activeClassName="nav-item active">Vault</NavLink></li>
-				<li><a className="nav-item" href="https://www.google.com" target="_blank">Documentation</a></li>
-			</ul>
-		</nav>
-	</header>
-);
+const Header: FC = () => {
+	const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+	return (
+		<header>
+			<nav>
+				<h1>
+					<Link to="/" className="home-link" title="Home">0x574r finance - testnet</Link>
+				</h1>
+				<ul>
+					<li><NavLink to="/stake" className="nav-item" activeClassName="nav-item active">Stake</NavLink></li>
+					<li><NavLink to="/vault" className="nav-item" activeClassName="nav-item active">Vault</NavLink></li>
+					<li><a className="nav-item" href="https://www.google.com" target="_blank">Documentation</a></li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
 export default Header;
--- src/ts/components/pages/Stake.tsx
+++ src/ts/components/pages/Stake.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
-import web3 from 'web3';
+import Web3 from 'web3';
 
-const Home: FC = () => (
-	<h2>Stake</h2>
-);
+const Home: FC = () => {
+	return (
+		<h2>Stake</h2>
+	);
+};
 
 export default Home;
