1a0bb93b8922f42c5cb51ef36f35b2448591a082
0xDaine <13499662+0xDaine@users.noreply.github.com>
2022-02-22 23:39:52 +0100

    added external menu link for future doc

--- src/ts/components/nav.tsx
+++ src/ts/components/nav.tsx
@@ -6,11 +6,12 @@ const Header: FC = () => (
 	<header>
 		<nav>
 			<h1>
-				<Link to="/" className="home-link" title="Home">0x574r finance</Link>
+				<Link to="/" className="home-link" title="Home">0x574r finance - testnet</Link>
 			</h1>
 			<ul>
 				<li><NavLink to="/stake" className="nav-item" activeClassName="nav-item active">Stake</NavLink></li>
 				<li><NavLink to="/vault" className="nav-item" activeClassName="nav-item active">Vault</NavLink></li>
+				<li><a className="nav-item" href="https://www.google.com" target="_blank">Documentation</a></li>
 			</ul>
 		</nav>
 	</header>
