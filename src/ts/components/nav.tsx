e7f5faf1e87a9516caaeec8246327ea19bdc28ac
0xDaine <13499662+0xDaine@users.noreply.github.com>
2022-02-24 00:16:34 +0100

    integrated web3modal

--- src/ts/components/nav.tsx
+++ src/ts/components/nav.tsx
@@ -1,19 +1,45 @@
 import React, { FC } from 'react';
-import { ConnectWallet } from "@3rdweb/react";
 import { Link, NavLink } from 'react-router-dom';
-import { useState } from "react"
+import { useState } from "react";
+import { HARMONY_TESTNET } from '../constant';
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from 'web3';
 
-class Header extends React.Component<Props, {}> {
-	
-
+class Header extends React.Component<Props_Nav, {}> {
 	/** constructor {{{ */
 	constructor(props: any) {
 		super(props);
 	}
 	/** }}} */
 
-	/** function: connect_wallet {{{ */
-	private connect_wallet = async (): Promise<void> => {
+	/** function: connect_wallet_click {{{ */
+	private connect_wallet_click = async (): Promise<void> => {
+		const providerOptions = {
+			walletconnect: {
+				package: WalletConnectProvider,
+				options: {
+					infuraId: "INFURA_ID"
+				}
+			}
+		};
+		const web3Modal = new Web3Modal({
+			cacheProvider: false,
+			providerOptions
+		});
+
+		web3Modal.clearCachedProvider();
+		const provider = await web3Modal.connect();
+		const web3 = new Web3(provider);
+	};
+	/** }}} */
+	/** function: connect_wallet_button {{{ */
+	private connect_wallet_button = (): JSX.Element => {
+		if (this.props.account.is_harmony) {
+			return (<button className="btn btn-success wallet-connect">Connected</button>);
+		}
+
+		return (<button className="btn btn-warning wallet-connect" onClick={ this.connect_wallet_click }>Connect wallet</button>);
 	};
 	/** }}} */
 
@@ -30,6 +56,7 @@ class Header extends React.Component<Props, {}> {
 					{/* <li><a className="nav-item" href="https://www.google.com" target="_blank">Documentation</a></li> */}
 					{/* <li><a className="nav-item" href="https://dexscreener.com/harmony" target="_blank">Chart</a></li> */}
 					<div className="connect">
+						{this.connect_wallet_button()}
 					</div>
 				</nav>
 			</header>
--- src/ts/components/router.tsx
+++ src/ts/components/router.tsx
@@ -5,17 +5,53 @@ import HttpNotFound from './pages/404';
 import Home from './pages/Home';
 import Stake from './pages/Stake';
 import Vault from './pages/Vault';
+import Web3 from 'web3';
 import { HARMONY_MAINNET, HARMONY_TESTNET } from '../constant';
+import { Web3ContextProvider } from "react-dapp-web3";
 
-class Routes extends React.Component<{}, {}> {
+class Routes extends React.Component<{}, State_Global> {
+	/** variables {{{ */
+	/** }}} */
+
 	/** constructor {{{ */
 	constructor(props: any) {
 		super(props);
+
+		this.state = {
+			account: {
+				is_harmony: false
+			}
+		};
 	}
+	/** }}} */
+
+	/** function: listen {{{ */
+	private listen = async (): Promise<void> => {
+		this.listen_wallet();
+	};
+	/** }}} */
+	/** function: listen_wallet {{{ */
+	private listen_wallet = (): void => {
+	};
+	/** }}} */
+
+	/** function: get_wallet {{{ */
+	private get_wallet = async (): Promise<void> => {
+	};
+	/** }}} */
+	/** function: get_network {{{ */
+	private get_network = async (): Promise<void> => {
+	};
+	/** }}} */
+
+	/** function: set_provider {{{ */
+	private set_provider = (provider: any): void => {
+	};
 	/** }}} */
 
 	/** function: componentDidMount {{{ */
 	componentDidMount = () => {
+		this.listen();
 	};
 	/** }}} */
 
@@ -23,7 +59,8 @@ class Routes extends React.Component<{}, {}> {
 	render() {
 		return (
 			<Router>
-				<Nav />
+				<Web3ContextProvider>
+					<Nav account={this.state.account} set_provider={this.set_provider} />
 					<main>
 						<Switch>
 							<Route exact path="/" ><Home /></Route>
@@ -32,6 +69,7 @@ class Routes extends React.Component<{}, {}> {
 							<Route><HttpNotFound /></Route>
 						</Switch>
 					</main>
+				</Web3ContextProvider>
 			</Router>
 		);
 	}
